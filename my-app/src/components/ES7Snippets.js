39a6071607b56b648ecfe679fd77fdfd3665b9e3
Kate Jones <katejones@kates-air.lan>
2023-10-08 13:50:18 +0100

    Add handy es7 snippets file

---
+++ my-app/src/components/ES7Snippets.js
@@ -0,0 +1,59 @@
+// Handy snippets
+
+// rfce - creates and exports a function component
+// 
+    import React from 'react'
+
+    function ES7Snippets() {
+    return (
+        <div>
+        
+        </div>
+    )
+    }
+
+export default ES7Snippets
+
+// rafce - creates and exports a function component as an arrow function
+// 
+    import React from 'react'
+
+    const ES7Snippets = () => {
+    return (
+        <div>
+        
+        </div>
+    )
+    }
+
+export default ES7Snippets
+
+// rce - creates and exports a class component
+// 
+    import React, { Component } from 'react'
+
+    export class ES7Snippets extends Component {
+    render() {
+        return (
+        <div>
+            
+        </div>
+        )
+    }
+    }
+
+    export default ES7Snippets
+
+// rconst - creates the constructor
+// 
+    constructor(props) {
+    super(props)
+
+    this.state = {
+        
+    }
+    }
+
+// clg - to console log
+// 
+    console.log(object)
