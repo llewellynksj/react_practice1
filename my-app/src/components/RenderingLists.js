a9c3772e04024d53a4dbffc0a435ca9598cd90f2
Kate Jones <katejones@kates-air.lan>
2023-10-09 12:42:50 +0100

    Rendering lists lesson

---
+++ my-app/src/components/RenderingLists.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import Book from './Book'
+
+function RenderingLists() {
+    const boookList = [
+        'to kill a mockingbird',
+        'the great gatsby',
+        'the catcher in the rye',
+    ]
+
+    const book = [
+        {
+            title: 'to kill a mockingbird',
+            author: 'harper lee',
+            pages: 281
+        },
+        {
+            title: 'the great gatsby',
+            author: 'f. scott fitzgerald',
+            pages: 218
+        },
+        {
+            title: 'the catcher in the rye',
+            author: 'j.d. salinger',
+            pages: 234
+        }
+    ]
+    return (
+    <div>
+        {boookList.map(book => {
+            return <h2>{book}</h2>
+        })}
+        <hr />
+        {
+            book.map(book => {
+                return <Book book={book} />
+            })
+        }
+    </div>
+  )
+}
+
+export default RenderingLists
